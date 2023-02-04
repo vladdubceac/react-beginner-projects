2ab5911361a9434ae22bc7f1a93c493e576bbf73
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-04 08:36:09 +0200

    First typescript project

---
+++ typescript-training/03-loops-and-arrays/sports.ts
@@ -0,0 +1,9 @@
+let sportsOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
+
+for (let tempSport of sportsOne) {
+  if (tempSport == "Cricket") {
+    console.log(tempSport + "<< My favorite sport");
+  } else {
+    console.log(tempSport);
+  }
+}
